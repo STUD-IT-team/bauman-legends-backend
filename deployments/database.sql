f5dc8d02b1f0b6efb28e96684c301e0fb05962d8
DVMelnikov <mit23560@gmail.com>
2023-09-21 23:53:40 +0300

    Edited database init script

--- deployments/database.sql
+++ deployments/database.sql
@@ -1,90 +1,125 @@
 CREATE TABLE teams (
 	line_num	SERIAL,
-	team_id		UUID	NOT NULL	PRIMARY KEY,
+	id		text	NOT NULL	PRIMARY KEY,
 	title	text	NOT NULL
 );
 
 CREATE TABLE roles (
-	id		SERIAL,
-	role_id	UUID		NOT NULL	PRIMARY KEY,
-	role_name	text	NOT NULL
+	line_num		SERIAL,
+	id	INT		NOT NULL	PRIMARY KEY,
+	title	text	NOT NULL
 );
 
 CREATE TABLE users (
-	id				SERIAL,
-	user_id 		UUID		NOT NULL	PRIMARY KEY,
+	line_num				SERIAL,
+	id 		text		NOT NULL	PRIMARY KEY,
+	password text	NOT NULL,
 	phone_number	text		NOT NULL,
 	email			text		NOT NULL,
 	email_confirmed	BOOLEAN		DEFAULT FALSE,
 	telegram		text		NOT NULL,
 	vk				text		NOT NULL,
 	study_group		text		NOT NULL,
-	full_name		text		NOT NULL,
-	team_id 		UUID 	    DEFAULT NULL,
+	fio		text		NOT NULL,
+	team_id 		text 	    DEFAULT NULL,
 	FOREIGN KEY (team_id) 
-		REFERENCES teams (team_id) 
+		REFERENCES teams (id) 
 			ON DELETE SET DEFAULT,
-	role_id			UUID 		DEFAULT NULL,
+	role_id			INT 		DEFAULT NULL,
 	FOREIGN KEY (role_id) 
-		REFERENCES roles (role_id) 
+		REFERENCES roles (id) 
 			ON DELETE SET DEFAULT,
-	admin_permissions	BOOLEAN	DEFAULT FALSE
+	is_admin	BOOLEAN	DEFAULT FALSE
 );
 
 CREATE TABLE types_tasks (
-	id	        SERIAL,
-	type_id		UUID		NOT NULL	PRIMARY KEY,
-	task_type	text		NOT NULL
+	line_num	        SERIAL,
+	id		INT		NOT NULL	PRIMARY KEY,
+	title	text		NOT NULL
 );
 
-CREATE TYPE type_difficulty AS ENUM ('1', '2', '3', '4', '5', '6', '7', '8', '9', '10');
+CREATE TABLE difficulties_tasks (
+	line_num	        SERIAL,
+	id		INT		NOT NULL	PRIMARY KEY,
+	title	text		NOT NULL
+);
 
 CREATE TABLE tasks (
-    id	        SERIAL,
-	task_id 	UUID	NOT NULL PRIMARY KEY,
-	task_name	text	NOT NULL,
+    line_num	        SERIAL,
+	id 	text	NOT NULL PRIMARY KEY,
+	title	text	NOT NULL,
     description	text,
-	correct_ans     text[]    DEFAULT array[]::text[],
-	qr_or_text_resp smallint  NOT NULL,
-    execution_time  text      NOT NULL,
-    task_difficulty type_difficulty  NOT NULL,
-    task_type_id    UUID,
-    FOREIGN KEY (task_type_id) 
-			REFERENCES types_tasks (type_id)
+	time_limit	text	NOT NULL,
+	difficulty_id			INT 		DEFAULT NULL,
+	FOREIGN KEY (difficulty_id) 
+		REFERENCES difficulties_tasks (id)
+			ON DELETE SET DEFAULT,
+	type_id			INT 		DEFAULT NULL,
+	FOREIGN KEY (type_id) 
+		REFERENCES types_tasks (id)
+			ON DELETE SET DEFAULT
 );
 
-CREATE TABLE ongoing_tasks (
-	id          	SERIAL,
-	ongoing_task_id	UUID		NOT NULL	PRIMARY KEY,
-   	task_id	    	UUID,
-    FOREIGN KEY (task_id) 
-		REFERENCES tasks (task_id),
-   	team_id     	UUID,
+CREATE TABLE team_tasks (
+	line_num        SERIAL,
+	id		text		NOT NULL,
+	task_id			text 		NOT NULL,
+	FOREIGN KEY (task_id) 
+		REFERENCES tasks (id),
+	team_id 		text 	    NOT NULL,
 	FOREIGN KEY (team_id) 
-		REFERENCES teams (team_id),
-    start_time  	TIMESTAMPTZ DEFAULT NOW(),
-    end_time    	TIMESTAMPTZ DEFAULT NULL,
-    extra_point 	INT         DEFAULT 0
+		REFERENCES teams (id),
+	start_time  	TIMESTAMPTZ DEFAULT NOW(),
+	end_time    	TIMESTAMPTZ DEFAULT NULL,
+	additional_points INT DEFAULT 0
+);
+
+CREATE TABLE types_answers (
+	line_num	        SERIAL,
+	id		INT		NOT NULL	PRIMARY KEY,
+	title	text		NOT NULL
+);
+
+CREATE TABLE answers_tasks (
+	line_num	        SERIAL,
+	id		text		NOT NULL,
+	task_id			text 		NOT NULL,
+	FOREIGN KEY (task_id) 
+		REFERENCES tasks (id),
+	answer_type_id	INT 		NOT NULL,
+	FOREIGN KEY (answer_type_id) 
+		REFERENCES types_answers (id),
+	data text NOT NULL
 );
 
 CREATE TABLE secrets (
-  	id              SERIAL,
-	secret_id		UUID	NOT NULL	PRIMARY KEY,
-    secret_name     text	NOT NULL,
-    description     text,
-    correct_ans     text[]           DEFAULT array[]::text[],
-    qr_or_text_resp smallint         NOT NULL
+	line_num	        SERIAL,
+	id		text		NOT NULL	PRIMARY KEY,
+	title	text NOT NULL,
+	description	text
 );
 
-CREATE TABLE ongoing_secrets (
-    id	SERIAL,
-	ongoing_secret_id	UUID	NOT NULL	PRIMARY KEY,
-    secret_id	UUID,
-    FOREIGN KEY (secret_id) 
-		REFERENCES secrets (secret_id),
-    team_id		UUID,
+CREATE TABLE answers_secrets (
+	line_num	        SERIAL,
+	id		text		NOT NULL,
+	secret_id			text 		NOT NULL,
+	FOREIGN KEY (secret_id) 
+		REFERENCES secrets (id),
+	answer_type_id	INT 		NOT NULL,
+	FOREIGN KEY (answer_type_id) 
+		REFERENCES types_answers (id),
+	data text NOT NULL
+);
+
+CREATE TABLE team_secrets (
+	line_num	        SERIAL,
+	id		text		NOT NULL,
+	secret_id 		text 	    NOT NULL,
+	FOREIGN KEY (secret_id) 
+		REFERENCES secrets (id),
+	team_id 		text 	    NOT NULL,
 	FOREIGN KEY (team_id) 
-		REFERENCES teams (team_id),
-    start_time  	TIMESTAMPTZ DEFAULT NOW(),
-    end_time    	TIMESTAMPTZ DEFAULT NULL
-)
+		REFERENCES teams (id),
+	start_time  	TIMESTAMPTZ DEFAULT NOW(),
+	end_time    	TIMESTAMPTZ DEFAULT NULL
+);
