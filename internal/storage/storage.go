b6fb280534621169b3d8c0a6f15459a7fdd60499
thirteenths <vera.korotkaya@yandex.ru>
2024-09-14 15:53:51 +0300

    rebase

--- internal/storage/storage.go
+++ internal/storage/storage.go
@@ -6,22 +6,25 @@ type Storage interface {
 	TeamStorage
 	ObjectStorage
 	UserStorage
+	MasterClassStorage
 }
 
 type storage struct {
-	TextTask  TextTaskStorage
-	MediaTask MediaTaskStorage
-	Team      TeamStorage
-	Object    ObjectStorage
-	User      UserStorage
+	TextTask    TextTaskStorage
+	MediaTask   MediaTaskStorage
+	Team        TeamStorage
+	Object      ObjectStorage
+	User        UserStorage
+	MasterClass MasterClassStorage
 }
 
-func NewStorage(team TeamStorage, textTask TextTaskStorage, mediaTask MediaTaskStorage, object ObjectStorage, user UserStorage) Storage {
+func NewStorage(team TeamStorage, textTask TextTaskStorage, mediaTask MediaTaskStorage, object ObjectStorage, user UserStorage, masterClass MasterClassStorage) Storage {
 	return &storage{
-		TextTask:  textTask,
-		Team:      team,
-		MediaTask: mediaTask,
-		Object:    object,
-		User:      user,
+		TextTask:    textTask,
+		Team:        team,
+		MediaTask:   mediaTask,
+		Object:      object,
+		User:        user,
+		MasterClass: masterClass,
 	}
 }
